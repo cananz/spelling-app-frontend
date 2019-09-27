517179d97cdc105bda86fc16d70818b8ca390373
github username <github email address>
2019-09-27 14:16:39 -0400

    play with html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 const phrasesURL = "http://localhost:3000/phrases"
+
+document.addEventListener("DOMContentLoaded", {
+  console.log("connected")
+  // randomPhrase()
+})
+
+function getPhrases(url){
+  randomPhrase
+  fetch(phrasesURL)
+  .then(r => r.json())
+  .then(r => {return r})
+}
+
+function randomPhrase(all){
+  randomPhrase()
+}
